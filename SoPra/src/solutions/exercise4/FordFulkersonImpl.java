945725f12f51e30ca44bdbfbf80d6565543b65b9
mehriban <mehriban.kurbanova@gmail.com>
2018-12-13 21:52:22 +0100

    Finish AB4

--- SoPra/src/solutions/exercise4/FordFulkersonImpl.java
+++ SoPra/src/solutions/exercise4/FordFulkersonImpl.java
@@ -3,7 +3,13 @@ package solutions.exercise4;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.Set;
 
 import org.sopra.api.exercises.ExerciseSubmission;
 import org.sopra.api.exercises.exercise3.FlowGraph;
@@ -11,8 +17,11 @@ import org.sopra.api.exercises.exercise3.ResidualEdge;
 import org.sopra.api.exercises.exercise3.ResidualGraph;
 import org.sopra.api.exercises.exercise4.FordFulkerson;
 
+import solutions.exercise3.ResidualEdgeImpl;
+import solutions.exercise3.ResidualGraphImpl;
+
 /**
- * @author Mehriban Malik
+ * @author Mehriban Kurbanova
  *
  */
 public class FordFulkersonImpl<V> implements FordFulkerson<V>, ExerciseSubmission {
@@ -21,51 +30,141 @@ public class FordFulkersonImpl<V> implements FordFulkerson<V>, ExerciseSubmissio
 		
 	}
 
+	/**
+	 * Finds the minimum capacity along the given path and adds the minimum capacity to flow of each edges of the path.
+	 * 
+	 * @param path Augmented path (represented by a Deque) where minimum capacity of path is added to flow
+	 * @throws java.lang.IllegalArgumentException - if parameter is null
+	 */
 	@Override
 	public void augmentPath(Deque<ResidualEdge<V>> path) {
 		if (path == null)
 			throw new IllegalArgumentException("Parameter path cannot be null");
 		
-		ArrayList<ResidualEdge<V>> pathAsArray = new ArrayList<ResidualEdge<V>>(path);	
+		ArrayList<ResidualEdge<V>> pathAsArray = new ArrayList<ResidualEdge<V>>(path);
+		
+		// first save capacities of all edges of the path
 		ArrayList<Integer> capacities = new ArrayList<Integer>();
 		for (ResidualEdge<V> edge: pathAsArray) {
 			capacities.add(edge.getCapacity());
 		}
 		
+		// then find the minimum and add it to the flows of all edges of the path
 		int minCapacity = Collections.min(capacities);
-		
 		for (ResidualEdge<V> edge : path) {
 			edge.addFlow(minCapacity);
 		}
 	}
 
+	/**
+	 * Computes the maximum flow in a flow network from start to target.
+	 * Given a flow network and a pair of nodes start & target, the algorithm calculates a maximum flow from start to target in given network.
+	 * 
+	 * @param graph Flow graph representing the flow network. Flows along edges in this graph will be set to max flows
+	 * @param start Source node
+	 * @param end Sink node
+	 * @throws java.lang.IllegalArgumentException - If any parameter is null
+	 * @throws java.util.NoSuchElementException - If start or target are not nodes in the graph
+	 */
 	@Override
 	public void findMaxFlow(FlowGraph<V> graph, V start, V end) {
 		if (start == null || end == null || graph == null) {
-			throw new IllegalArgumentException("");
+			throw new IllegalArgumentException("Argument should not be null");
+		}
+		Set<V> nodes = graph.getNodes();
+		if (!nodes.contains(start) || !nodes.contains(end)) {
+			throw new NoSuchElementException("One of the give node is not in the graph.");
 		}
 		
+		// this is an implementation of the formal FF-algorithm
+		// found at http://www.inf.ufpr.br/pos/techreport/RT_DINF003_2004.pdf
+		ResidualGraph<V> resGraph = new ResidualGraphImpl<V>(graph);
+		
+		Deque<ResidualEdge<V>> shortestPath = findPath(start, end, resGraph);
+		
+		while (shortestPath != null) {
+			augmentPath(shortestPath);
+		}
 	}
 
+	/**
+	 * Finds the shortest path in the residual graph from and to the given nodes.
+	 * The path is computed using the breadth-first search.
+	 * 
+	 * @param start the start node of the path
+	 * @param end the end node of the path
+	 * @param graph the residual graph
+	 * @return a deque containing the shortest path in the reversed order.
+	 * @throws java.lang.IllegalArgumentException - If at least one of the parameters is null
+	 */
 	@Override
 	public Deque<ResidualEdge<V>> findPath(V start, V end, ResidualGraph<V> graph) {
 		if (start == null || end == null || graph == null) {
 			throw new IllegalArgumentException("Parameter(s) can not be null!");
 		}
-		//Vergleich seiten 10-15 in "Folien04.pdf"
-		Deque<ResidualEdge<V>> path = new LinkedList<ResidualEdge<V>>(); //Erreichbarkeitsbaum
-		Deque<V> queue = new LinkedList<V>(); //Warteschlange
 		
-		queue.add(start); //Startknoten zur Warteschlange hinzufuegen
-		queue.poll(); //ersten knoten der Warteschlange entfernen unt returnen
-		path.addAll(graph.edgesFrom(start)); //alle kanten ausgehend vom erste knoten dem erreichbarkeitsbaum hinzufuegen
-		
-		return path;
+		Map<V, Boolean> visited = new HashMap<V, Boolean>(); // saves already visited nodes
+		Map<V, V> edgesFromStart = new HashMap<V, V>(); // contains edges for the current while step
+	    Queue<V> queue = new LinkedList<V>();
+	    
+	    V current = start;
+	    queue.add(current);
+	    visited.put(current, true); // we mark the start node as visited
+	    while(!queue.isEmpty()) {
+	        current = queue.remove();
+	        
+	        // if the end node is found, the shortest path is already found
+	        if (current.equals(end)) {
+	            break;
+	        }
+	        else {
+	        	// first retrieve all children for the current node, edges to children
+	        	// should have capacity > 0
+	        	List<ResidualEdge<V>> currentEdges = graph.edgesFrom(current);
+	        	List<V> childrenNodes = new ArrayList<V>();
+            	for (ResidualEdge<V> edge : currentEdges) {
+            		if (edge.getCapacity() > 0 && current != edge.getEnd()) {
+            			childrenNodes.add(edge.getEnd());
+            		}
+            	}
+	        	
+            	// once children are found, add them to the queue, mark as visited
+            	// and keep track of the edges in the reversed order
+	            for(V node :childrenNodes){
+	            	// an already visited child will not be considered anymore
+	                if(!visited.containsKey(node)){
+	                    queue.add(node);
+	                    visited.put(node, true);
+	                    edgesFromStart.put(node, current);
+	                }
+	            }
+	        }
+	    }
+	    
+	    // if last node was not the end node, then the path was not found
+	    if (!current.equals(end)){
+	        return null;
+	    }
+	    
+	    // extract the path to the list in the reversed order
+	    List<V> pathAsList = new LinkedList<V>();
+	    for(V node = end; node != null; node = edgesFromStart.get(node)) {
+	        pathAsList.add(node);
+	    }
+	    
+	    // convert the list to deque with residual edges
+	    Deque<ResidualEdge<V>> result = new LinkedList<ResidualEdge<V>>();
+	    ResidualEdge<V> edge;
+	    for (int i = 0; i <= pathAsList.size() - 2; i++) {
+	    	edge = new ResidualEdgeImpl<V>(pathAsList.get(i+1), pathAsList.get(i), graph.getEdge(pathAsList.get(i+1), pathAsList.get(i)).getCapacity());
+	    	result.add(edge);
+	    }
+	    
+	    return result;
 	}
 
 	@Override
 	public String getTeamIdentifier() {
 		return "G06T02";
 	}
-
 }
--- SoPra/src/solutions/exercise4/FordFulkersonTest.java
+++ SoPra/src/solutions/exercise4/FordFulkersonTest.java
@@ -1,20 +1,27 @@
 package solutions.exercise4;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
-
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.Test;
 import org.sopra.api.exercises.ExerciseSubmission;
+import org.sopra.api.exercises.exercise3.FlowEdge;
 import org.sopra.api.exercises.exercise3.ResidualEdge;
 import org.sopra.api.exercises.exercise3.ResidualGraph;
 import org.sopra.api.exercises.exercise4.AbstractFordFulkersonTest;
 
+import solutions.exercise3.FlowEdgeImpl;
 import solutions.exercise3.ResidualGraphImpl;
 
+
 /**
- * @author Mehriban Malik
+ * @author Mehriban Malik, Adrian Paquin
  *
  */
 public class FordFulkersonTest extends AbstractFordFulkersonTest implements ExerciseSubmission {
@@ -24,11 +31,11 @@ public class FordFulkersonTest extends AbstractFordFulkersonTest implements Exer
 		
 	}
 	
-	public void printDeque(Deque<ResidualEdge<String>> que) {
-		for (ResidualEdge<String> edge : que) {
-			System.out.print(edge.getCapacity() + " ");
-		}
-		System.out.println("");
+	@Test
+	public void testOurMaxFlow() {
+		FordFulkersonImpl<String> f = new FordFulkersonImpl<String>();
+		
+		f.findMaxFlow(flowGraphA, "s", "t");
 	}
 
 	@Override
@@ -43,8 +50,6 @@ public class FordFulkersonTest extends AbstractFordFulkersonTest implements Exer
 		testPath.add(resGraph1.getEdge("a", "d"));
 		testPath.add(resGraph1.getEdge("d", "t"));
 		
-		printDeque(testPath);
-		
 		sut.augmentPath(testPath);
 		
 		Integer[] correctValues = {4, 0, 5, 3};
@@ -56,8 +61,6 @@ public class FordFulkersonTest extends AbstractFordFulkersonTest implements Exer
  			
 			current_index++;
 		}
-		
-		printDeque(testPath);
 	}
 
 	@Override
@@ -73,8 +76,6 @@ public class FordFulkersonTest extends AbstractFordFulkersonTest implements Exer
 		testPath.add(resGraph2.getEdge("b", "e"));
 		testPath.add(resGraph2.getEdge("e", "t"));
 		
-		printDeque(testPath);
-		
 		sut.augmentPath(testPath);
 		
 		Integer[] correctValues = {7, 2, 0, 4, 2};
@@ -86,110 +87,436 @@ public class FordFulkersonTest extends AbstractFordFulkersonTest implements Exer
  			
 			current_index++;
 		}
-		
-		printDeque(testPath);
 	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_augmentPath_ParameterNull() {
-		try {
-			sut.augmentPath(null);
-			fail("Argument should not be null");
-		} catch (IllegalArgumentException ex) {
-			
+		sut.augmentPath(null);
 		}
-	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_findMaxFlow_ParameterGraphIsNull() {
-		// TODO Auto-generated method stub
-		
+		sut.findMaxFlow(null, "s", "t");
 	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_findMaxFlow_ParameterStartIsNull() {
-		// TODO Auto-generated method stub
-		
+		sut.findMaxFlow(flowGraphA, null, "t");		
 	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_findMaxFlow_ParameterTargetIsNull() {
-		// TODO Auto-generated method stub
-		
+		sut.findMaxFlow(flowGraphA, "s", null);		
 	}
 
 	@Override
-	@Test
+	@Test (expected = NoSuchElementException.class)
 	public void test_findMaxFlow_ParameterTargetNotInGraph() {
-		// TODO Auto-generated method stub
-		
+		String target = "Target";
+		sut.findMaxFlow(flowGraphA, "s", target);		
 	}
 
 	@Override
 	@Test
 	public void test_findMaxFlow_flowGraphA() {
-		// TODO Auto-generated method stub
+		List<FlowEdge<String>> expectedResult = new ArrayList<FlowEdge<String>>();
 		
+		FlowEdge<String> expectedEdge = new FlowEdgeImpl<String>("a", "b", 11);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "s", 15);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "c", 3);
+		expectedEdge.setFlow(3);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "d", 6);
+		expectedEdge.setFlow(6);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "a", 11);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "s", 20);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "c", 4);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "e", 8);
+		expectedEdge.setFlow(8);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("s", "a", 15);
+		expectedEdge.setFlow(9);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("s", "b", 20);
+		expectedEdge.setFlow(8);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "a", 3);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "b", 4);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "d", 6);
+		expectedEdge.setFlow(2);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "e", 1);
+		expectedEdge.setFlow(1);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "a", 6);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "c", 6);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "t", 7);
+		expectedEdge.setFlow(7);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "e", 7);
+		expectedEdge.setFlow(1);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("t", "d", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("t", "e", 10);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "b", 8);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "c", 1);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "d", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "t", 10);
+		expectedEdge.setFlow(10);
+		expectedResult.add(expectedEdge);
+		
+		sut.findMaxFlow(flowGraphA, "s", "t");
+		
+		List<FlowEdge<String>> testResult = flowGraphA.getEdges();
+		int index = 0;
+		for (FlowEdge<String> edge : testResult) {
+			assertEquals(edge.getStart(), expectedResult.get(index).getStart());
+			assertEquals(edge.getEnd(), expectedResult.get(index).getEnd());
+			assertEquals(edge.getCapacity(), expectedResult.get(index).getCapacity());
+			assertEquals(edge.getFlow(), expectedResult.get(index).getFlow());
+			index++;
+		}
+
 	}
 
 	@Override
 	@Test
 	public void test_findMaxFlow_flowGraphB() {
-		// TODO Auto-generated method stub
+		List<FlowEdge<String>> expectedResult = new ArrayList<FlowEdge<String>>();
 		
+		FlowEdge<String> expectedEdge = new FlowEdgeImpl<String>("a", "b", 5);
+		expectedEdge.setFlow(4);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "s", 16);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "c", 7);
+		expectedEdge.setFlow(7);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "d", 3);
+		expectedEdge.setFlow(3);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "a", 5);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "s", 10);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "c", 7);
+		expectedEdge.setFlow(4);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "e", 10);
+		expectedEdge.setFlow(10);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("s", "a", 16);
+		expectedEdge.setFlow(14);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("s", "b", 10);
+		expectedEdge.setFlow(10);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "a", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "b", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "d", 11);
+		expectedEdge.setFlow(11);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "a", 3);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "c", 11);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "t", 14);
+		expectedEdge.setFlow(14);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "e", 9);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("t", "d", 14);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("t", "e", 12);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "b", 10);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "d", 9);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "t", 12);
+		expectedEdge.setFlow(10);
+		expectedResult.add(expectedEdge);
+		
+		sut.findMaxFlow(flowGraphB, "s", "t");
+		
+		List<FlowEdge<String>> testResult = flowGraphB.getEdges();
+		int index = 0;
+		for (FlowEdge<String> edge : testResult) {
+			assertEquals(edge.getStart(), expectedResult.get(index).getStart());
+			assertEquals(edge.getEnd(), expectedResult.get(index).getEnd());
+			assertEquals(edge.getCapacity(), expectedResult.get(index).getCapacity());
+			assertEquals(edge.getFlow(), expectedResult.get(index).getFlow());
+			index++;
+		}
+
 	}
 
 	@Override
 	@Test
 	public void test_findMaxFlow_flowGraphC() {
-		// TODO Auto-generated method stub
+		List<FlowEdge<String>> expectedResult = new ArrayList<FlowEdge<String>>();
+		FlowEdge<String> expectedEdge = new FlowEdgeImpl<String>("a", "b", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
 		
+		expectedEdge = new FlowEdgeImpl<String>("a", "s", 11);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("a", "c", 6);
+		expectedEdge.setFlow(6);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "a", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "s", 5);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("b", "c", 4);
+		expectedEdge.setFlow(4);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("s", "a", 11);
+		expectedEdge.setFlow(6);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("s", "b", 5);
+		expectedEdge.setFlow(4);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "a", 6);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "b", 4);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "d", 11);
+		expectedEdge.setFlow(7);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("c", "e", 3);
+		expectedEdge.setFlow(3);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "c", 11);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "t", 6);
+		expectedEdge.setFlow(6);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("d", "e", 8);
+		expectedEdge.setFlow(1);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("t", "d", 6);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("t", "e", 7);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "c", 3);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "d", 8);
+		expectedEdge.setFlow(0);
+		expectedResult.add(expectedEdge);
+		
+		expectedEdge = new FlowEdgeImpl<String>("e", "t", 7);
+		expectedEdge.setFlow(4);
+		expectedResult.add(expectedEdge);
+		
+		sut.findMaxFlow(flowGraphC, "s", "t");
+		
+		List<FlowEdge<String>> testResult = flowGraphC.getEdges();
+		int index = 0;
+		for (FlowEdge<String> edge : testResult) {
+			assertEquals(edge.getStart(), expectedResult.get(index).getStart());
+			assertEquals(edge.getEnd(), expectedResult.get(index).getEnd());
+			assertEquals(edge.getCapacity(), expectedResult.get(index).getCapacity());
+			assertEquals(edge.getFlow(), expectedResult.get(index).getFlow());
+			index++;
+		}
 	}
 
 	@Override
 	@Test
 	public void test_findPath1() {
-		// TODO Auto-generated method stub
+		ResidualGraph<String> resGraph1 = new ResidualGraphImpl<String>(flowGraph1);
+		Deque<ResidualEdge<String>> foundPath = sut.findPath("s", "t", resGraph1);
 		
+		assertEquals(foundPath.size(), 3);
+		ResidualEdge<String> edge = foundPath.removeLast();
+		assertEquals(edge.getStart(), "s");
+		assertEquals(edge.getEnd(), "a");
+		assertEquals(edge.getCapacity(), 10);
+		
+		edge = foundPath.removeLast();
+		assertEquals(edge.getStart(), "a");
+		assertEquals(edge.getEnd(), "d");
+		assertEquals(edge.getCapacity(), 8);
+		
+		edge = foundPath.removeLast();
+		assertEquals(edge.getStart(), "d");
+		assertEquals(edge.getEnd(), "t");
+		assertEquals(edge.getCapacity(), 6);
 	}
 
 	@Override
 	@Test
 	public void test_findPath2() {
-		// TODO Auto-generated method stub
+		ResidualGraph<String> resGraph2 = new ResidualGraphImpl<String>(flowGraph2);
+		Deque<ResidualEdge<String>> foundPath = sut.findPath("s", "t", resGraph2);
 		
+		assertEquals(foundPath.size(), 3);
+		ResidualEdge<String> edge = foundPath.removeLast();
+		assertEquals(edge.getStart(), "s");
+		assertEquals(edge.getEnd(), "b");
+		assertEquals(edge.getCapacity(), 11);
+		
+		edge = foundPath.removeLast();
+		assertEquals(edge.getStart(), "b");
+		assertEquals(edge.getEnd(), "e");
+		assertEquals(edge.getCapacity(), 9);
+		
+		edge = foundPath.removeLast();
+		assertEquals(edge.getStart(), "e");
+		assertEquals(edge.getEnd(), "t");
+		assertEquals(edge.getCapacity(), 7);
 	}
 
 	@Override
 	@Test
 	public void test_findPath_IsNull() {
-		// TODO Auto-generated method stub
+		Deque<ResidualEdge<String>> testPath = new LinkedList<ResidualEdge<String>>();
+		ResidualGraph<String> resGraph3 = new ResidualGraphImpl<String>(flowGraph3);
 		
+		testPath.add(resGraph3.getEdge("s", "a"));
+		testPath.add(resGraph3.getEdge("a", "c"));
+		testPath.add(resGraph3.getEdge("c", "e"));
+		testPath.add(resGraph3.getEdge("e", "t"));
+		Deque<ResidualEdge<String>> foundPath = sut.findPath("s", "t", resGraph3);
+		assertNull(foundPath);
 	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_findPath_ParameterGraphIsNull() {
-		// TODO Auto-generated method stub
-		
+		sut.findPath("s", "t", null);		
 	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_findPath_ParameterStartIsNull() {
-		// TODO Auto-generated method stub
-		
+		ResidualGraph<String> resGraph = new ResidualGraphImpl<String>(flowGraph1);
+		sut.findPath(null, "t", resGraph);		
 	}
 
 	@Override
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void test_findPath_ParameterTargetIsNull() {
-		// TODO Auto-generated method stub
-		
+		ResidualGraph<String> resGraph = new ResidualGraphImpl<String>(flowGraph1);
+		sut.findPath("s", null, resGraph);		
 	}
 
 	@Override
